6b1272532acd76e65211505a53c74dc3241f6890
MCTzOCK <dev@mctzock.de>
2021-01-11 18:13:20 +0100

    Added Prefix

--- src/main/java/net/craftions/skywars/SkyWars.java
+++ src/main/java/net/craftions/skywars/SkyWars.java
@@ -4,10 +4,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SkyWars extends JavaPlugin {
 
+    public static final String prefix = "[§eSky§cWars§r] ";
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-
     }
 
     @Override
