1029e0152e19a221ff681167ba0e04c0944fa58d
MCTzOCK <dev@mctzock.de>
2021-01-11 18:11:11 +0100

    first commit

---
+++ src/main/java/net/craftions/skywars/SkyWars.java
@@ -0,0 +1,17 @@
+package net.craftions.skywars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkyWars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
