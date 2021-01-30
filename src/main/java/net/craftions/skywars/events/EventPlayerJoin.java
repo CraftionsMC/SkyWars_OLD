a51dcfe43aa8fdae45aea0bb3664bfc5cd0ab713
Sausefix <dev@craftions.net>
2021-01-30 20:44:10 +0100

    started kits

--- src/main/java/net/craftions/skywars/events/EventPlayerJoin.java
+++ src/main/java/net/craftions/skywars/events/EventPlayerJoin.java
@@ -12,7 +12,6 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.graalvm.compiler.phases.schedule.SchedulePhase;
 
 public class EventPlayerJoin implements Listener {
 
